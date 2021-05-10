1213a57e4bc190dfd2a0dd344f80e645ad3b208e
Bishko86 <polroldon@gmail.com>
2021-05-10 16:43:35 +0300

    added useState condition, that returns content if state is empty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import VisibleTodoList from './containers/VisibleTodoList';
 import Footer from './components/Footer/Footer';
 import AddTodo from './components/AddTodo/AddTodo';
 import Header from './components/Header/Header';
+import { connect } from 'react-redux';
 
 
-const App = () => (
-  <div className={'baseWrap'}>
-    <Header />
-    <div>
-      <AddTodo />
-      <VisibleTodoList />
-    </div>
-    <Footer />
+const App = ({ todos }) => {
 
-  </div>
+  let [emptyListMode, setEmptyListMode] = useState(false);
 
-)
+  useEffect(() => {
+    if (todos.length > 0) setEmptyListMode(false);
+    else setEmptyListMode(true);
+  })
 
-export default App;
+  return (
+    < div className={'baseWrap'} >
+      <Header />
+
+      <div className={'currentWrap'}>
+        <AddTodo />
+        {emptyListMode ? <h2>Your todo list is empty.</h2> :
+          <div>
+            <Footer />
+            <VisibleTodoList />
+          </div>}
+      </div>
+
+
+    </div >
+  )
+}
+function mapStateToProps(state) {
+  return {
+    todos: state.todos
+  }
+}
+export default connect(mapStateToProps, null)(App);
